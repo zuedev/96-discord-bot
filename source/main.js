9ee353fabe25094babf2277f6d493b25c49b7a79
Alex Pooley <zuedev@gmail.com>
2025-03-27 01:05:21 +0000

    refactor cron job setup to use sendReminderCron function for better readability and maintainability

--- source/main.js
+++ source/main.js
@@ -45,51 +45,41 @@ discord.login(process.env.DISCORD_BOT_TOKEN);
 
 async function cronSetup() {
   // Weekdays @ 0900 CST: Wake up!
-  new CronJob(
+  sendReminderCron(
     "0 9 * * 1-5",
-    async () => {
-      let message = "Good morning! ☀️ Time to wake up! ⏰";
-      console.log(`Sending message to client: ${message}`);
-      await state.client.send(message);
-      await state.manager.send(`Sent message to client: ${message}`);
-      console.log("Message sent!");
-    },
-    null,
-    true,
+    "Good morning! ☀️ Time to wake up! ⏰",
     "America/Chicago"
   );
 
   // Monday, Wednesday, and Thursday @ 1855 CST: Stream prep
-  new CronJob(
+  sendReminderCron(
     "55 18 * * 1,3,4",
-    async () => {
-      let message = "# Stream Prep Time! 🛠️";
-      message += "\n\n";
-      message += "Stream starts in 5 minutes!";
-      message += "\n";
-      message += "## Checklist 📋";
-      message += "\n";
-      message += "- Go live on Twitter 🐦";
-      message += "\n";
-      message += "- Go Post to all the socials 📣";
-      message += "\n";
-      message += "- Refill water 💧";
-      message += "\n";
-      console.log(`Sending message to client: ${message}`);
-      await state.client.send(message);
-      await state.manager.send(`Sent message to client: ${message}`);
-      console.log("Message sent!");
-    },
-    null,
-    true,
+    [
+      "# Stream Prep Time! 🛠️",
+      "",
+      "Stream starts in 5 minutes!",
+      "## Checklist 📋",
+      "- Go live on Twitter 🐦",
+      "- Go Post to all the socials 📣",
+      "- Refill water 💧",
+    ],
     "America/Chicago"
   );
 
   // Monday, Wednesday, and Thursday @ 1900 CST: Stream start
-  new CronJob(
+  sendReminderCron(
     "0 19 * * 1,3,4",
+    "Time to go live! 🚀 Good luck! ❤️",
+    "America/Chicago"
+  );
+}
+
+function sendReminderCron(cron, message, timzone = "Etc/UTC") {
+  if (typeof message === "array") message = message.join("\n");
+
+  new CronJob(
+    cron,
     async () => {
-      let message = "Time to go live! 🚀 Good luck! ❤️";
       console.log(`Sending message to client: ${message}`);
       await state.client.send(message);
       await state.manager.send(`Sent message to client: ${message}`);
@@ -97,6 +87,6 @@ async function cronSetup() {
     },
     null,
     true,
-    "America/Chicago"
+    timezone
   );
 }
