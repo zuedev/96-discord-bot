a19a8e71b6df70cc88aff3720db4a48630f316be
Alex <zuedev@gmail.com>
2025-04-15 14:51:49 +0100

    Update talent.js

--- source/data/talent.js
+++ source/data/talent.js
@@ -10,49 +10,49 @@ export default [
   {
     name: "YayJayBae",
     discord_id: "246758317433683969",
-    // cronjobs: [
-    //   // Weekdays @ 0900 CST: Wake up!
-    //   [
-    //     "0 9 * * 1-5",
-    //     "Good morning! ☀️ Time to wake up! ⏰ Make sure you eat before 10am! 🍳🥞🥓",
-    //     "America/Chicago",
-    //   ],
-    //   // Weekdays @ 1300 CST: Eat lunch!
-    //   ["0 13 * * 1-5", "Time to eat lunch! 🍔🥗🍕", "America/Chicago"],
-    //   // Monday, Wednesday, and Thursday @ 1800 CST: Nap if you need it!
-    //   [
-    //     "0 18 * * 1,3,4",
-    //     "It's one hour before stream time! If you need a nap, now's the time! 😴",
-    //     "America/Chicago",
-    //   ],
-    //   // Monday, Wednesday, and Thursday @ 1900 CST: Stream start
-    //   [
-    //     "0 19 * * 1,3,4",
-    //     [
-    //       "# Stream Prep Time! 🛠️",
-    //       "Time to go live! 🚀 Good luck! ❤️",
-    //       "## Checklist 📋",
-    //       "- Go Post to all the socials 📣",
-    //       "- Refill water 💧",
-    //       "- Check audio levels 🎙️",
-    //     ],
-    //     "America/Chicago",
-    //   ],
-    //   // Monday, Wednesday, and Thursday @ 1910 CST: Go live on Twitter and YouTube
-    //   [
-    //     "10 19 * * 1,3,4",
-    //     "Go live on Twitter and YouTube! 🐦📹",
-    //     "America/Chicago",
-    //   ],
-    //   // Tuesday, Friday, and Sunday @ 1900 CST: Post to socials
-    //   [
-    //     "0 19 * * 2,5,0",
-    //     [
-    //       "Time to post to your 4 main socials!",
-    //     ],
-    //     "America/Chicago",
-    //   ],
-    // ],
+    cronjobs: [
+      // Weekdays @ 0900 CST: Wake up!
+      [
+        "0 9 * * 1-5",
+        "Good morning! ☀️ Time to wake up! ⏰ Make sure you eat before 10am! 🍳🥞🥓",
+        "America/Chicago",
+      ],
+      // Weekdays @ 1300 CST: Eat lunch!
+      ["0 13 * * 1-5", "Time to eat lunch! 🍔🥗🍕", "America/Chicago"],
+      // Monday, Wednesday, and Thursday @ 1800 CST: Nap if you need it!
+      [
+        "0 18 * * 1,3,4",
+        "It's one hour before stream time! If you need a nap, now's the time! 😴",
+        "America/Chicago",
+      ],
+      // Monday, Wednesday, and Thursday @ 1900 CST: Stream start
+      [
+        "0 19 * * 1,3,4",
+        [
+          "# Stream Prep Time! 🛠️",
+          "Time to go live! 🚀 Good luck! ❤️",
+          "## Checklist 📋",
+          "- Go Post to all the socials 📣",
+          "- Refill water 💧",
+          "- Check audio levels 🎙️",
+        ],
+        "America/Chicago",
+      ],
+      // Monday, Wednesday, and Thursday @ 1910 CST: Go live on Twitter and YouTube
+      [
+        "10 19 * * 1,3,4",
+        "Go live on Twitter and YouTube! 🐦📹",
+        "America/Chicago",
+      ],
+      // Tuesday, Friday, and Sunday @ 1900 CST: Post to socials
+      [
+        "0 19 * * 2,5,0",
+        [
+          "Time to post to your 4 main socials!",
+        ],
+        "America/Chicago",
+      ],
+    ],
   },
   {
     name: "Bunn",
@@ -73,5 +73,9 @@ export default [
   {
     name: "Woney",
     discord_id: "1113687477937512478",
+    cronjobs: [
+      // Weekdays @ 1900 EST: Work on new model debut
+      ["0 19 * * 1-5", "**REMINDER:** Work on new model debut!", "America/New_York"],
+    ],
   },
 ];
